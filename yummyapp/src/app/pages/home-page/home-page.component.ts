066fd7b288945c4e958c44b1ed8cfcc79a48d9cc
akvitko <alexey.kvitko.15@gmain.com>
2018-12-13 21:17:16 +0300

    add menu

--- yummyapp/src/app/pages/home-page/home-page.component.ts
+++ yummyapp/src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state, style, animate, transition, animation, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  animations: [
+    trigger('triggerBackgroundChange', [
+      transition('* => 1', [
+        style({ opacity: 0.5 }),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition('* => 2',  [
+        animate(1000, style({ opacity: 0.5 })),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition('* => 3',  [
+        animate(1000, style({ opacity: 0.5 })),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition('* => 4',  [
+        animate(1000, style({ opacity: 0.5 })),
+        animate(1000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class HomePageComponent implements OnInit {
+  homeImgSrc: string;
+  imageNum: number = 1;
 
   constructor() {
   }
 
   ngOnInit() {
-    console.log("index init");
+    this.changeBackground();
   }
+
+  changeBackground(){
+    this.imageNum ++;
+    this.homeImgSrc = 'assets/images/home-'+this.imageNum+'.jpg';
+    if( this.imageNum == 5 ){
+      this.imageNum = 1;
+    }
+  }
+
+
 }
