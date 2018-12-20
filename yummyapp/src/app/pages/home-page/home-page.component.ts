0249a17277ec01e28fe390295d020d9d311bdf42
akvitko <kpk_08@mail.ru>
2018-12-20 17:26:00 +0300

    add anima event

--- yummyapp/src/app/pages/home-page/home-page.component.ts
+++ yummyapp/src/app/pages/home-page/home-page.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {CATALOG} from '../../pages/home-page/catalog';
 import {animate, state, style, transition, trigger} from "@angular/animations";
 import {TrackScrollDirective} from "../../directives/track-scroll";
-import {take} from 'rxjs/operators';
-import {timer} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
@@ -15,7 +13,7 @@ import {timer} from "rxjs";
         opacity: '0',
         top: '-75px',
         width: '130px',
-        left: '15%'
+        left: '20%'
       })),
       state('final', style({
         opacity: '1',
@@ -25,66 +23,33 @@ import {timer} from "rxjs";
       })),
       transition('initial=>final', animate('400ms')),
       transition('final=>initial', animate('200ms'))
-    ]),
-    trigger('changeInvaitePosition', [
-      state('inviteOne', style({
-        opacity: '1',
-        left: '80%'
-      })),
-      state('inviteTwo', style({
-        opacity: '0',
-        left: '80%'
-      })),
-      state('inviteThree', style({
-        opacity: '0',
-        left: '15%'
-      })),
-      state('inviteFour', style({
-        opacity: '1',
-        left: '15%'
-      })),
-      transition('inviteOne=>inviteTwo', animate('1000ms')),
-      transition('inviteTwo=>inviteThree', animate('600ms')),
-      transition('inviteThree=>inviteFour', animate('1000ms')),
-      transition('inviteFour=>inviteOne', animate('1000ms')),
     ])
   ]
 })
 export class HomePageComponent {
   @ViewChild(TrackScrollDirective) scroll: TrackScrollDirective;
-  inviteImgSrc: string = 'assets/images/invite.png';
+  mainScrImg: string = 'assets/images/shahlyk-1.jpg';
   animArr: string[] = ['inviteOne','inviteTwo','inviteThree','inviteFour'];
   invitePos: string = 'inviteOne';
   pos: number = 1;
   logoImgSrc: string = 'assets/images/logo.png';
   currentState = 'initial';
   catalogItems = CATALOG;
-  inviteTimer = timer(1500, 2500);
-
-  imageUrlArray: string[] = ['assets/images/slide-2.jpg',
-    'assets/images/back-1.jpg','assets/images/slide-1.jpg','assets/images/back-3.jpg'];
+  inviteOpacity: number = 1;
 
   scrollPercent: number = 0;
 
   constructor() {
-    // timer(1000, 5000).pipe(
-    //   take(10000))
-    this.inviteTimer.subscribe(x => {
-      this.divClick();
-    })
-  }
-
-  divClick() {
-    if( this.pos == 4){
-      this.pos = 0;
-    }
-    this.invitePos = this.animArr[this.pos];
-    this.pos++;
   }
 
 
   track(value: number): void {
-    if (value > 15) {
+    let calc = 1- value/25;
+    if( calc < 0 ){
+      calc = 0;
+    }
+    this.inviteOpacity = calc;
+    if (value > 20) {
       this.currentState = 'final';
     } else {
       this.currentState = 'initial';
