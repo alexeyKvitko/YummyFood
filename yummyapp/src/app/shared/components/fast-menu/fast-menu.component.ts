4ccb57d9fa1d9943e3dfe90c6580f81414d0844d
akvitko <kpk_08@mail.ru>
2019-02-04 17:08:46 +0300

    snapshot

--- yummyapp/src/app/shared/components/fast-menu/fast-menu.component.ts
+++ yummyapp/src/app/shared/components/fast-menu/fast-menu.component.ts
@@ -19,9 +19,9 @@ export class FastMenuComponent implements OnInit {
         this.topPos = data.value+'px';
         this.top = (+data.value);
         this.fmOpacity = 1;
-        if ( this.top < 94 ){
-          this.fmOpacity = 1 - (this.top /100);
-        }
+        // if ( this.top < 94 ){
+        //   this.fmOpacity = 1 - (this.top /100);
+        // }
       }
     }, error => {
       console.log('Error: ' + error);
